b9670d78d1583c07f50282018c70d937390e5023
szymonaszek01 <235879@edu.p.lodz.pl>
2023-05-23 17:56:49 +0200

    task-005 Testing connection to local database

--- app/src/main/java/com/app/toDo/MainActivity.java
+++ app/src/main/java/com/app/toDo/MainActivity.java
@@ -1,14 +1,25 @@
 package com.app.toDo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.app.toDo.configuration.DatabaseConfiguration;
+import com.app.toDo.entity.Category;
+
+import java.util.concurrent.Executors;
+
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseConfiguration databaseConfiguration = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        databaseConfiguration = DatabaseConfiguration.getInstance(getApplicationContext());
+        Category category = Category.builder().name("Sport").id(3).build();
+        Executors.newSingleThreadExecutor().execute(() -> databaseConfiguration.categoryDao().insertCategory(category));
     }
 }
