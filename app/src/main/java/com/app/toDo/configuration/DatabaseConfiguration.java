77319896f8a141314bcef8efc85181931bcdd6a7
szymonaszek01 <235879@edu.p.lodz.pl>
2023-05-23 17:56:16 +0200

    task-004 Adding "Constant" and "Database configuration" to connect with local database

---
+++ app/src/main/java/com/app/toDo/configuration/DatabaseConfiguration.java
@@ -0,0 +1,37 @@
+package com.app.toDo.configuration;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.app.toDo.dao.CategoryDao;
+import com.app.toDo.dao.NotificationDao;
+import com.app.toDo.dao.TaskDao;
+import com.app.toDo.entity.Category;
+import com.app.toDo.entity.Notification;
+import com.app.toDo.entity.Task;
+import com.app.toDo.util.ConstantUtil;
+
+@Database(entities = {Task.class, Category.class, Notification.class}, exportSchema = false, version = 1)
+public abstract class DatabaseConfiguration extends RoomDatabase {
+
+    private static DatabaseConfiguration databaseConfiguration;
+
+    public static synchronized DatabaseConfiguration getInstance(Context context) {
+        if (databaseConfiguration == null) {
+            databaseConfiguration = Room.databaseBuilder(context, DatabaseConfiguration.class, ConstantUtil.DB_NAME)
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+
+        return databaseConfiguration;
+    }
+
+    public abstract TaskDao taskDao();
+
+    public abstract CategoryDao categoryDao();
+
+    public abstract NotificationDao notificationDao();
+}
---
+++ app/src/main/java/com/app/toDo/util/ConstantUtil.java
@@ -0,0 +1,6 @@
+package com.app.toDo.util;
+
+public class ConstantUtil {
+
+    public static final String DB_NAME = "to_do_app_db";
+}
